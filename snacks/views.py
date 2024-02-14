3a5ab91208e267bc379a4563826575b40219c9d7
evidad <errolvidad@gmail.com>
2024-02-14 15:30:30 -0800

    added styling

--- snacks/views.py
+++ snacks/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-# from django.shortcuts import render
+from django.shortcuts import render
 from .models import Snack
 from django.urls import reverse_lazy
 
@@ -29,3 +29,9 @@ class SnackDeleteView(DeleteView):
     template_name = 'snack_delete.html'
     model = Snack
     success_url = reverse_lazy('snack_list')
+
+def about_me(request):
+    context = {
+        'image_url': '.assets/PXL_20230816_231329155.MP.jpg',
+    }
+    return render(request, 'about_me.html', context)
